70d760381221cad78f4c1cd95d12ac62cf175f9d
Badru <badruakfm@gmail.com>
2021-02-18 09:57:38 +0700

    end 8 list

--- src/App.js
+++ src/App.js
@@ -41,12 +41,25 @@ function App() {
     setCards(cards_copy);
   };
 
+  const changeNameHandler = (event,id) => {
+    //1. which card
+    const cardIndex = cards.findIndex(card => card.id == id);
+
+    //2. make a copy of the cards
+    const cards_copy = [...cards]
+    //3. change the name of the spesifics
+    cards_copy[cardIndex].name = event.target.value
+    //4. set the cards with the latest version of card copy
+    setCards(cards_copy)
+  }
+
   const cardsMarkup = showCard && (cards.map((card, index) => <Card
     avatar={card.avatar}
     name={card.name}
     title={card.title}
     key={card.id}
     onDelete={() => {deleteCardHandler(index)}}
+    onChangeName = { (event) => changeNameHandler(event,card.id) }
   />)
   );
 
