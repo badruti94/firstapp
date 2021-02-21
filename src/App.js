a764f09657bb18baa7277bbeb547f7685ac85d31
Badru <badruakfm@gmail.com>
2021-02-21 22:18:00 +0700

    10 react hooks : 49. useEffect fetch a card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Card from './Card';
 import { AvatarGenerator } from 'random-avatar-generator';
 import { ThemeProvider } from 'styled-components'
-import Button from './element/Button'
 import axios from 'axios'
 
 const theme = {
@@ -11,76 +10,38 @@ const theme = {
   mango: 'yellow'
 }
 
-
-const generator = new AvatarGenerator();
-
-console.log(generator.generateRandomAvatar())
-
-
-
 function App() {
 
-
-  const [cards, setCards] = useState([])
-  const [showCard, setShowCard] = useState(true);
+  const [card, setCard] = useState([])
+  const [id, setId] = useState(1);
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(res => {
-      console.log(res);
-      setCards(res.data)
-    })
-  },[])
-
-
-  const toggleShowCard = () => setShowCard(!showCard);
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards];
-    cards_copy.splice(cardIndex, 1);
-    console.log('cards_copy', cards_copy)
-    console.log('cards', cards)
-    setCards(cards_copy);
-  };
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => {
+        console.log(res);
+        setCard(res.data)
+      })
+  }, [id])
 
   const changeNameHandler = (event, id) => {
-    //1. which card
-    const cardIndex = cards.findIndex(card => card.id === id);
-
-    //2. make a copy of the cards
-    const cards_copy = [...cards]
-    //3. change the name of the spesifics
-    cards_copy[cardIndex].name = event.target.value
-    //4. set the cards with the latest version of card copy
-    setCards(cards_copy)//
+    //1. make a copy of the cards
+    const card_copy = {...card}
+    //2. change the name of the spesifics
+    card_copy.name = event.target.value
+    //3. set the cards with the latest version of card copy
+    setCard(card_copy)//
   }
-
-  /* const buttonStyle = {
-    backgroundColor : null
-  } */
-
-  const classes = ['button']
-
-
-  if (cards.length < 3) classes.push('pink');
-  if (cards.length < 2) classes.push('red text');
-
-  console.log(classes);
-
-  const cardsMarkup = showCard && (cards.map((card, index) => <Card
-    name={card.name}
-    phone={card.phone}
-    key={card.id}
-    onDelete={() => { deleteCardHandler(index) }}
-    onChangeName={(event) => changeNameHandler(event, card.id)}
-  />)
-  );
 
   return (
     <ThemeProvider theme={theme} >
       <div className="App">
-        <Button color="mango" length={cards.length} >Toggle</Button>
-        <button className={classes.join(' ')} onClick={toggleShowCard} >Toggle show/hide</button>
-        {cardsMarkup}
+        <input type="text" value={id} onChange={e=>setId(e.target.value)} />
+        <Card
+          name={card.name}
+          phone={card.phone}
+          key={card.id}
+          onChangeName={(event) => changeNameHandler(event, card.id)}
+        />
       </div>
     </ThemeProvider>
   );
