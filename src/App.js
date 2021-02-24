2c7deafaccb40ecc7cb3454f5c075bad7513a792
Badru <badruakfm@gmail.com>
2021-02-24 21:00:01 +0700

    10 react hooks : 56. useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import  Counter from "./component/Counter";
+import './component/ComponentA'
+import ComponentA from './component/ComponentA';
+export const CounterContext = React.createContext()
+
+const initialState = {
+  counter: 0,
+
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, counter: state.counter + action.payload }
+    case 'decrement':
+      return { ...state, counter: state.counter - action.payload }
+    case 'reset':
+      return initialState
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <CounterContext.Provider value={{ counter: state.counter, dispatch }} >
+
+      <div className="App">
+        App js {state.counter}
+        <ComponentA />
+      </div>
+    </CounterContext.Provider>
+
+
   );
 }
 
---
+++ src/component/ComponentA.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { Button, ButtonGroup, Badge } from "reactstrap";
+import { CounterContext } from '../App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const ComponentA = () => {
+    const counterContext = useContext(CounterContext)
+    console.log(counterContext)
+
+    const {counter, dispatch} = counterContext
+
+    return (
+        <div>
+            <ButtonGroup>
+                <Button color="primary" outline >
+                    ComponentA Counter1 <Badge color="secondary" >{counter}</Badge>
+                </Button>
+            </ButtonGroup>
+            <p></p>
+            <ButtonGroup>
+                <Button color="dark" onClick={() => dispatch({ type: 'increment', payload: 1 })} >increment</Button>
+                <Button color="dark" onClick={() => dispatch({ type: 'decrement', payload: 1 })} >decrement</Button>
+                <Button color="danger" onClick={() => dispatch({ type: 'reset' })} >reset</Button>
+            </ButtonGroup>
+            <p></p>
+
+        </div>
+
+    )
+}
+
+export default ComponentA
