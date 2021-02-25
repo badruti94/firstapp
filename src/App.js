f9eb692261530e3e8f9f2d53fbb5ed5cc10cad3d
Badru <badruakfm@gmail.com>
2021-02-25 20:27:34 +0700

    10 react hooks : 63. useRef - deal with the unmount component problems

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import {useRef  } from "react";
+import {useRef, useState  } from "react";
+import Component from "./components/Component";
 import './App.css';
+
 
 function App() {
   const inputRef = useRef()
+  const [show, setShow] = useState(true)
+
   return (
     <div className="App">
+      <h1>Focus on input field</h1>
       <input ref={inputRef} type="text"/>
       <input type="text"/>
       <input type="text"/>
       <button onClick={()=>{
         inputRef.current.focus()
       }} >focus</button>
+      <p></p>
+      <button onClick={() => setShow(!show)} >Toggle Components</button>
+      <h1>Unmount Component</h1>
+      { show && <Component /> }
     </div>
   );
 }
