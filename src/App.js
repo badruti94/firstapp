77bbe2b3237b33503e472bf6a8cad2205ee1f2f4
Badru <badruakfm@gmail.com>
2021-02-25 19:54:02 +0700

    10 react hooks : 60. useMemo 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useState, useMemo } from 'react';
 import './App.css';
 import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
 
 function App() {
   const [countA, setCountA] = useState(0)
+  const [countB, setCountB] = useState(0)
   const incrementA = () => {
     setCountA(countA + 1)
   };
+  const incrementB = () => {
+    setCountB(countB + 1)
+  };
 
   const memoComponentA = useMemo(() => {
-    return <ComponentA />
-  },[])
+    return <ComponentA count={countA} />
+  },[countA])
 
   return (
     <div className="App">
@@ -18,7 +23,11 @@ function App() {
       <p>
         <button onClick={incrementA} >Increment A</button>
       </p>
+      <p>
+        <button onClick={incrementB} >Increment B</button>
+      </p>
       {memoComponentA}
+      <ComponentB count={countB} />
     </div>
   );
 }
--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,6 +1,6 @@
 import {useEffect} from 'react'
 let renderCount = 1
-const ComponentA = (props) => {
+const ComponentB = (props) => {
     useEffect(() => {
         renderCount++
     })
@@ -8,10 +8,10 @@ const ComponentA = (props) => {
     return (
         <div>
             <h1>
-                ComponentA : render {renderCount} times || Counter : {props.count}
+                ComponentB : render {renderCount} times || Counter : {props.count}
             </h1>
         </div>
     )
 }
 
-export default ComponentA
+export default ComponentB
