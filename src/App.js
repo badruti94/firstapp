a8534e80d8b195cd0691dfb9a764f01c301b4277
Badru <badruakfm@gmail.com>
2021-02-25 21:31:27 +0700

    10 react hooks : 64. useCallback - to prevent the recreation of function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import {useRef, useState  } from "react";
-import Component from "./components/Component";
+import { useState, useCallback } from 'react'
 import './App.css';
-
-
+const functionCounter = new Set()
 function App() {
-  const inputRef = useRef()
-  const [show, setShow] = useState(true)
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+  const increment = () => {
+    console.log('increment');
+    setCount(count + 1)
+  }
+  const increment2 =  useCallback(() => {
+    console.log('increment2');
+    setCount2(count2 + 1)
+  }, [count2])
+
+  functionCounter.add(increment)
+  functionCounter.add(increment2)
+  console.log(functionCounter);
 
   return (
     <div className="App">
-      <h1>Focus on input field</h1>
-      <input ref={inputRef} type="text"/>
-      <input type="text"/>
-      <input type="text"/>
-      <button onClick={()=>{
-        inputRef.current.focus()
-      }} >focus</button>
-      <p></p>
-      <button onClick={() => setShow(!show)} >Toggle Components</button>
-      <h1>Unmount Component</h1>
-      { show && <Component /> }
+      <p>counter 1 : {count} </p> <p> counter 2 : {count2} </p>
+      <button onClick={increment} >increase counter 1</button>
+      <button onClick={increment2} >increase counter 2</button>
     </div>
   );
 }
 
 export default App;
+
