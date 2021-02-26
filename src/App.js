45209f16449c933cfd78503e5d526b69a78cb99c
Badru <badruakfm@gmail.com>
2021-02-26 22:59:32 +0700

    11 react router : 73. Fetch specific post and using Switch from React Router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import Post from './components/Post';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Route exact path='/' component={Home} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/about' component={About} />
-        <Route path='/:post_id' component={Post} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/about' component={About} />
+          <Route path='/:post_id' component={Post} />
+        </Switch>
+
       </div>
     </Router>
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Post = (props) => {
     console.log(props);
     const id = props.match.params.post_id
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            setPost(res.data)
+        })()
+
+    }, [])
+
     return (
         <div>
             <div className="card">
                 <div className="card-header">
-                    {id}
+                    {post.title}
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text">{post.body}</p>
                 </div>
             </div>
         </div>
