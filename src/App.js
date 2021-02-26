581e2944275313d4f240df132b8009683d67c098
Badru <badruakfm@gmail.com>
2021-02-26 20:17:34 +0700

    11 react router : 67. React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Form from './components/Form';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/about' component={About} />
+      </div>
+    </Router>
+
   );
 }
 
