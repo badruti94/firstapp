067b5ff7bd96af01196f3a002e11a4b823613c40
Badru <badruakfm@gmail.com>
2021-02-27 17:58:16 +0700

    12 redux : 82. Dispatch the login action

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './action';
+import { increment, decrement, auth } from './action';
 import './App.css';
 
 function App() {
   const counter = useSelector(state => state.counter)
   const login = useSelector(state => state.login)
   const dispatch = useDispatch()
+  const authText = login? 'logout' : 'login'
   return (
     <div className="App">
       <h1>Counter {counter} </h1>
       <button onClick={()=>dispatch(increment())} >+</button>
       <button onClick={()=>dispatch(decrement())} >-</button>
       <p> { login ? <h1>Logined</h1> : <h1>Not yet Logined</h1> } </p>
+      <button onClick={()=>dispatch(auth())} > {authText} </button>
     </div>
   );
 }
